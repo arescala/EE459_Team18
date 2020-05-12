7da04c10b076a492b326420c03a1dda3656c32a6
Michael Tawata <admin@michaeltawata.com>
2020-05-12 16:09:39 -0700

    created prototype of led_driver

--- led_driver.h
+++ led_driver.h
@@ -1,6 +1,17 @@
 #ifndef LED_DRIVER_H
 #define LED_DRIVER_H
 
+#include <stdlib.h>
+#include <avr/io.h>
 
+#include "PCA9685/PCA9685_driver.h"
+
+//all of the indicators expect uint8_t
+//with 0 being 0% and 256 being 100%
+
+void set_speed_indicator(uint8_t speed);
+void set_pan_indicator(uint8_t pan);
+void set_tilt_indicator(uint8_t tilt);
+void set_battery_indicator(uint8_t battery);
 
 #endif
